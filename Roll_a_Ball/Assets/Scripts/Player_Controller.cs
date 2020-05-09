28bd8a40dc6eaac438067f5760c29a96fa63736d
xastar888 <58084044+xastar888@users.noreply.github.com>
2020-05-09 00:08:22 -0400

    Exam 2
    Yay!!!

---
+++ Roll_a_Ball/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Controller : MonoBehaviour
+{
+
+
+
+    public float speed;
+    public Text CountText;
+    public Text WinText;
+
+
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
+
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+
+        float moveHori = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 Movement = new Vector3(moveHori, 0.0f, moveVert);
+
+        rb.AddForce(Movement * speed);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+        void SetCountText () {
+            CountText.text = "Count:" + count.ToString();
+        if (count >= 6){
+            WinText.text = "You Win!";
+        }
+        }
+
+    }
+
+
